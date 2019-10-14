93282fed1abc2227535b1e0ee71793fe08d5c62f
AndreyGalchevski <andreygalchevski@gmail.com>
2019-10-14 12:01:11 +0300

    change app.listen() call

--- src/app.ts
+++ src/app.ts
@@ -32,10 +32,4 @@ app.use('/api/members', memberRouter);
 app.use('/api/songs', songRouter);
 app.use('/api/videos', videoRouter);
 
-app.listen(port, err => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`server is listening on ${port}`);
-});
+app.listen(port);
